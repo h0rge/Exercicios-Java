1e272f3e5a0d95c28e0ffe1cf3f9e60cf004e39b
Jorge Lago Álvarez <jorge.lago.alvarez@gmail.com>
2026-03-03 14:27:10 +0100

    Exercise 1 done and 2 started

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,45 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        //Exercicio 1: Crear un array unidimensional
+        Scanner sc = new Scanner(System.in);
+        double[] grades;
+        int length;
+        System.out.println("How many grades do you want to store?");
+        length = sc.nextInt();
+        grades = new double[length];
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println("Introduce unha nota");
+            grades[i] = sc.nextInt();
         }
+        // Average
+        double total = 0;
+        for (double grade : grades) {
+            total += grade;
+        }
+        System.out.println("A media de todas as notas é " + total / grades.length);
+
+        // Exercicio 2: Arrays impares e pares
+
+        int[] even;
+        int[] odd;
+
+        even = new int[100];
+        odd = new int[100];
+
+        for (int i = 0; i < 200; i++) {
+
+        }
+
+
+
+        // Código ata aquí
     }
 }
+
