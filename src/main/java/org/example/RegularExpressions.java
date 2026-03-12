06b446a2608936db0ea0dd92c35fa855a45a0eb8
Jorge Lago Álvarez <jorge.lago.alvarez@gmail.com>
2026-03-12 13:39:51 +0100

    Class RegEx added

--- src/main/java/org/example/RegularExpressions.java
+++ src/main/java/org/example/RegularExpressions.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class RegularExpressions {
+    /*
+    * Create the following regular expressions and provide matching and non-matching examples:
+
+    * Regular Expression	                        Regular expression	    Matching Example	    Non-Matching Example
+    * Three uppercase letters                       [A-Z]{3}
+    * 9-digit number                                \\d{9}
+    * Number, period, and
+    * uppercase/lowercase letter                    (0-9)(\.)(A-Z|a-z)
+    * Uppercase initial and lowercase               ^[A-Z][a-z]+
+    * Does not start with a digit                   ^\\D
+    * Between 5 and 10 alphanumeric characters      \\w{5,10}
+    * Starts with [DAW] followed
+    * by three numbers between 4 and 7              ^(\\[DAW\\])[4-7]{3}
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    * */
 }
